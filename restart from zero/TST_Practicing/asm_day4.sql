21f531ee48cf9260f63e0fa2f35b4d2886b53686
trinhsytoi1412 <trinhsytoi1412@gmail.com>
2025-04-22 11:31:48 +0700

    update file day4 sql

--- restart from zero/TST_Practicing/asm_day4.sql
+++ restart from zero/TST_Practicing/asm_day4.sql
@@ -31,20 +31,6 @@ CREATE TABLE employees (
     manager_id INTEGER
 );
 
-delete from employees;
-
-alter table employees
-add constraint fk_manager foreign key (manager_id) references manager(id) on delete cascade ;
-
-create table manager(
-    id serial primary key ,
-    name varchar(255)
-);
-
-insert into manager(name)
-values ('Nguyễn Văn A'),
-       ('Trần Thị B');
-
 -- Dữ liệu mẫu
 INSERT INTO customers (name, email) VALUES
 ('Alice', 'alice@example.com'),
@@ -78,36 +64,38 @@ INSERT INTO employees (name, manager_id) VALUES
 
 
 --1.	Viết câu lệnh SQL để liệt kê danh sách tên khách hàng và ngày đặt hàng.
-select name, o.order_date
-    from customers
-    inner join orders o on customers.id = o.customer_id;
+select c.name as customer_name, o.order_date
+    from customers c
+    inner join orders o on c.id = o.customer_id;
 
 --2.	Liệt kê toàn bộ khách hàng, kèm theo tổng tiền đơn hàng nếu có. Nếu khách chưa có đơn hàng thì để trống phần tổng tiền.
-select name, email, o.total_amount
-    from customers
-    left join orders o on customers.id = o.customer_id;
+select c.name as customer_name, o.total_amount
+    from customers c
+    left join orders o on c.id = o.customer_id;
 
 --3.	Liệt kê danh sách đơn hàng và tên khách hàng tương ứng. Nếu đơn hàng không rõ khách thì vẫn hiển thị.
-select order_date, customer_id, total_amount, c.name
-    from orders
-    left join customers c on c.id = orders.id;
+select o.id as order_id, c.name as customer_name
+    from orders o
+    right join customers c on o.customer_id = c.id;
 
 --4.	Tạo một danh sách kết hợp cả khách hàng và đơn hàng, dù có liên kết hay không.
-select * from customers
-    full join orders o on customers.id = o.customer_id;
+select c.name as customer_name, o.id as order_id
+    from customers c
+    full join orders o on c.id = o.customer_id;
 
 --5.	Tạo danh sách kết hợp mọi sản phẩm với mọi đơn hàng (dùng cho thử nghiệm/test case).
-select * from orders
-    full join order_items oi on oi.order_id = orders.id
-    full join products p on oi.product_id = p.id;
+select o.id as order_id, p.name as product_name
+    from orders o
+    cross join products p;
 
 --6.	Viết câu SQL liệt kê tên nhân viên và tên quản lý của họ.
-select * from employees
-    full join manager m on m.id = employees.manager_id;
+select e.name as employee, m.name as manager
+    from employees e
+    left join employees m on e.manager_id = m.id;
 
 --7.	Viết câu lệnh SQL liệt kê: Tên khách hàng, Ngày đặt hàng, Tên sản phẩm, Số lượng
-select customers.name, o.order_date, p.name, oi.quantity
-from customers
-    inner join orders o on customers.id = o.customer_id
+select c.name as customer_name, o.order_date, p.name as product_name, oi.quantity
+from customers c
+    inner join orders o on c.id = o.customer_id
     inner join order_items oi on o.id = oi.order_id
-    inner join products p on p.id = oi.product_id;
+    inner join products p on oi.product_id = p.id;
